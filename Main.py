61c5a816ecf7cdc759d0caf05d55db67faa09dac
Gorczycki <95108053+Gorczycki@users.noreply.github.com>
2024-07-02 19:19:45 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -30,12 +30,12 @@ time_steps = int(T / dt) #dynamic t
 num_brownian_motions = 5
 
 
-def W_t(num_brownian_motions):
+
+
+def W_t(num_brownian_motions, time_steps, dt):
     rng = numpy.random.default_rng(72)
     Z = rng.normal(0, 1, (num_brownian_motions, time_steps))
-    W = numpy.cumsum(numpy.sqrt(dt) * Z)
-    
-
+    W = numpy.cumsum(numpy.sqrt(dt) * Z, axis = 1)
     return W
     #returns the wiener process as vector.
 
@@ -48,15 +48,9 @@ def stockbrownianmotion(S_0, stock_mu, W, stock_sigma, dt):
     #from the simplification of BS, mu and sigma taken as constants, when in reality they are not.
     #dS(t) = mu*S(t)dt + sigma*S(t)dW(t)
     #S(t) = S(0)exp{(mu - (sigma^2)/2)*t + sigma*W(t)}
-    #W_t2 = W_t1 + sqrt(t_2 - t_1)*Z
-    
-    
-    t = numpy.linspace(0, T, time_steps)
+    #W_t2 = W_t1 + sqrt(t_2 - t_1)*Z    
+    t = numpy.linspace(0, T, len(W))
     S = S_0 * numpy.exp((stock_mu - 0.5 * stock_sigma**2) * t + stock_sigma*W)
-
-
-
-    #Now make GBM with S(t) equation
    
 
     return S
@@ -79,9 +73,9 @@ def optionpricedelta(S, K, T, r, stock_sigma):
 
 
 
-W = W_t(num_brownian_motions)
+W = W_t(num_brownian_motions, time_steps, dt)
 
-stock_prices = numpy.array([stockbrownianmotion(S_0, stock_mu, stock_sigma, W, dt) for i in range(num_brownian_motions)])
+stock_prices = numpy.array([stockbrownianmotion(S_0, stock_mu, W[i], stock_sigma, dt) for i in range(num_brownian_motions)])
 
 
 # Calculate call option prices over time
@@ -102,26 +96,28 @@ for i in range(num_brownian_motions):
 # Plotting
 plt.figure(figsize=(12, 12))
 
-# Plot Wiener process
 plt.subplot(3, 1, 1)
-plt.plot(numpy.linspace(0, T, time_steps), W)
+for i in range(num_brownian_motions):
+    plt.plot(numpy.linspace(0, T, time_steps), W[i])
 plt.title('Wiener Process')
 plt.xlabel('Time')
 plt.ylabel('W(t)')
 
-# Plot stock price
 plt.subplot(3, 1, 2)
-plt.plot(numpy.linspace(0, T, time_steps), stock_prices)
+for i in range(num_brownian_motions):
+    plt.plot(numpy.linspace(0, T, time_steps), stock_prices[i])
 plt.title('Stock Price (Geometric Brownian Motion)')
 plt.xlabel('Time')
 plt.ylabel('S(t)')
 
-# Plot call option price
+
 plt.subplot(3, 1, 3)
-plt.plot(numpy.linspace(0, T, len(call_prices)), call_prices)
+for i in range(num_brownian_motions):
+    plt.plot(numpy.linspace(0, T, len(call_prices[i])), call_prices[i])
 plt.title('Call Option Price')
 plt.xlabel('Time')
 plt.ylabel('Call Price')
 
 plt.tight_layout()
 plt.show()
+
